ab75fcb4973bf991c478cf2b95570db867206869
Daphineatim <hopedaphine19@gmail.com>
2022-10-13 17:22:35 +0300

    Fix linters

--- modules/addScore.js
+++ modules/addScore.js
@@ -1,5 +1,6 @@
 import Score from './score.js';
-const apiURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/BWGz2P57jaQba1AheDjm/scores/'
+
+const apiURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/BWGz2P57jaQba1AheDjm/scores/';
 
 const fetchAPI = async () => {
   const response = await fetch(apiURL);
